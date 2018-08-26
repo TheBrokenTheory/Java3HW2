6f82588d104fc2a1be9906a6541afebed5761351
Jonathan <Jonathananders6@gmail.com>
2018-08-26 12:30:46 -0400

    Init

---
+++ Homework2/src/homework2/Homework2.java
@@ -0,0 +1,16 @@
+package homework2;
+
+/**
+ *
+ * @author Jonathan Anders
+ */
+public class Homework2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
